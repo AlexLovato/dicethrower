61462b091c995f325f4f28932c3b4a8edd0ecaf1
Alex Lovato <alex.diego.lovato@gmail.com>
2014-06-27 08:53:24 -0600

    w/ comments
    comments

--- dicethrower1000/Program.cs
+++ dicethrower1000/Program.cs
@@ -9,8 +9,10 @@ namespace dicethrower1000
     class Program
     {
         static void Main(string[] args)
-        {
+        {//tell them the format
             Console.WriteLine("type a format like 6d7 where 7 is the number of sides and 6 is the number of times rolled");
+
+            //recieve their input
           string input =  Console.ReadLine();
             Console.WriteLine("rolling: " + input);
             changeInput(input);
@@ -18,10 +20,12 @@ namespace dicethrower1000
         }
         static void changeInput(string dice)
         {
+            //set up variables
             string firstNumber = null;
             string secondNumber1 = null;
             string secondnumber2 = null;
            var diceList = dice.ToList();
+            //take apart the input 
             foreach (char a in diceList)
             {
                 if (a == 'd')
@@ -31,7 +35,7 @@ namespace dicethrower1000
                 else {firstNumber = firstNumber + a;}
             }
             for (int i = diceList.Count-1; i > 0; i-- )
-            {
+            {//break apart at the D to separate the numbers
                 if (diceList[i] == 'd')
                 {
                     break;
@@ -41,17 +45,19 @@ namespace dicethrower1000
             for (int i = secondNumber1.Length-1; i >= 0; i--)
             {
                 secondnumber2 = secondnumber2 + secondNumber1[i];
-            }
+            }//confirm their numbers just to clarify
             Console.WriteLine("first number = " + firstNumber + "second number is = " + secondnumber2);
             Console.ReadKey();
             int timesRolled = int.Parse(firstNumber);
             int numSides = int.Parse(secondnumber2);
+            //make em ints
+            //make a random number in specified size in specified number of rolls
             for (int i = 0; i < timesRolled; i++)
             {
                 Random randomNumberGenerator = new Random();
                 int randomNumber = randomNumberGenerator.Next(1, numSides);
                 int rollCount = i + 1;
-                Console.WriteLine("Results per roll");
+              //display info
                 Console.WriteLine("Roll " + rollCount + " = " + randomNumber);
                 Console.ReadKey();
             }
